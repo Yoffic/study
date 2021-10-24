217a86e0d99db90588f4cf0bf1f624e1f406c123
Olga Ioffe <yoffic@yahoo.com>
2021-10-24 10:37:53 +0400

    add replace array method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = 0
+        array.each { |item| max = item if item > max }
+        array.map { |item| item > 0 ? max : item }
       end
 
       def search(_array, _query)
