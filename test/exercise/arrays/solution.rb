c30b225ba9fd8ad403c763497cb4813158a43f65
Olga Ioffe <yoffic@yahoo.com>
2021-10-24 10:38:38 +0400

    add binary search array method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -8,7 +8,25 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+        def binary_search(items, counter, query)
+          if items.empty?
+            return -1
+          end
+          half_count = items.length / 2
+          left = items[0, half_count]
+          right = items[half_count..]
+          if left.last == query
+            return counter + half_count - 1
+          elsif right.first == query
+            return counter + half_count
+          end
+          if left.last && left.last < query
+            binary_search(right, counter + half_count, query)
+          else
+            binary_search(left, counter, query)
+          end
+        end
+        binary_search(_array, 0, _query)
       end
     end
   end
