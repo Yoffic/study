cc801b711dffcae810454440012a02384fb60d3e
Olga Ioffe <yoffic@yahoo.com>
2021-11-07 20:39:29 +0400

    add solution to rating and chars_count methods

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,21 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        has_many_countries = ->(movie) { movie['country'] && movie['country'].split(',').length > 1 }
+        rating_to_count = array.reduce([]) { |movies, movie| has_many_countries.call(movie) ? movies << movie : movies }
+                               .map { |movie| movie['rating_kinopoisk'].to_f || 0 }
+                               .reduce([]) { |ratings, current_rating| current_rating.positive? ? ratings << current_rating : ratings }
+        rating_sum = rating_to_count.reduce(0) { |sum, rating| sum + rating }
+        rating_sum / rating_to_count.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        has_needed_rating = ->(movie) { movie['rating_kinopoisk'] && movie['rating_kinopoisk'].to_f >= threshold }
+        films
+          .reduce([]) { |rated_movies, movie| has_needed_rating.call(movie) ? rated_movies << movie : rated_movies }
+          .map { |movie| movie['name'].count 'и' }
+          .reduce(0) { |chars_sum, chars_count| chars_sum + chars_count }
       end
     end
   end
