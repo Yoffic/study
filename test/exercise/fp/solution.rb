25fa94a6ede7faa71319f00c39bcdbafd02e4352
Olga Ioffe <yoffic@yahoo.com>
2021-11-09 11:16:03 +0400

    refactor rating and chars_count functions to filter values first

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,7 +6,7 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         has_many_countries = ->(movie) { movie['country'] && movie['country'].split(',').length > 1 }
-        rating_to_count = array.reduce([]) { |movies, movie| has_many_countries.call(movie) ? movies << movie : movies }
+        rating_to_count = array.select(&has_many_countries)
                                .map { |movie| movie['rating_kinopoisk'].to_f || 0 }
                                .reduce([]) { |ratings, current_rating| current_rating.positive? ? ratings << current_rating : ratings }
         rating_sum = rating_to_count.reduce(0) { |sum, rating| sum + rating }
@@ -16,7 +16,7 @@ module Exercise
       def chars_count(films, threshold)
         has_needed_rating = ->(movie) { movie['rating_kinopoisk'] && movie['rating_kinopoisk'].to_f >= threshold }
         films
-          .reduce([]) { |rated_movies, movie| has_needed_rating.call(movie) ? rated_movies << movie : rated_movies }
+          .select(&has_needed_rating)
           .map { |movie| movie['name'].count 'и' }
           .reduce(0) { |chars_sum, chars_count| chars_sum + chars_count }
       end
