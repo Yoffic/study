26f4292bfe0b9d4abdc3f6c16be885f4b71bfbcb
Olga Ioffe <yoffic@yahoo.com>
2021-11-13 10:24:16 +0400

    add my_each my_map my_compact my_reduce methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,41 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def apply_block(items, block)
+        return if items.empty?
+        first, *rest = items
+        block.call(first)
+        apply_block(rest, block)
+      end
+
+      def reduce_with_block(result, items, block)
+        return result if items.empty?
+        first, *rest = items
+        new_result = result.nil? ? first : block.call(result, first)
+        reduce_with_block(new_result, rest, block)
+      end
+
+      def my_each(&block)
+        apply_block(self, block)
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        create_new_collection = ->(result, item) { result << block.call(item) }
+        my_reduce(self.class.new, &create_new_collection)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        create_new_collection = ->(result, item) { !item.nil? ? result << item : result }
+        my_reduce(self.class.new, &create_new_collection)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(start = nil, &block)
+        reduce_with_block(start, self, block)
+      end
     end
   end
 end
